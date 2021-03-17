6af4b4b7aa8ed8337cb3e5fa28fc54fd8991f049
hitallocavas <hcs3@cin.ufpe.br>
2021-03-17 00:09:13 -0300

    Adicionando outros classificadores

--- process_data.py
+++ process_data.py
@@ -2,10 +2,16 @@ import os
 import json
 import nltk, re, pprint
 from bs4 import BeautifulSoup
+import time
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -70,22 +76,67 @@ def naive_bayes(X_train, X_test, y_train, y_test):
     classifier = GaussianNB()
     classifier.fit(X_train, y_train)
 
-    # Predict Class
     y_pred = classifier.predict(X_test)
 
+    print_metrics(y_pred, y_test, 'Naive Bayes')
+
+
+def support_vector_machine(X_train, X_test, y_train, y_test):
+    classifier = svm.SVC(kernel='linear')  # Linear Kernel
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print_metrics(y_pred, y_test, 'Support Vector Machine')
+
+
+def decision_trees(X_train, X_test, y_train, y_test):
+    classifier = DecisionTreeClassifier()
+    classifier = classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print_metrics(y_pred, y_test, 'Decision Trees (JV8)')
+
+
+def logistic_regression(X_train, X_test, y_train, y_test):
+    classifier = LogisticRegression()
+    classifier = classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print_metrics(y_pred, y_test, 'Logistic Regression')
+
+
+def multilayer_perceptron(X_train, X_test, y_train, y_test):
+    classifier = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+    classifier = classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print_metrics(y_pred, y_test, 'MultiLayer Perceptron')
+
+
+def print_metrics(y_pred, y_test, classification_method):
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
-    ftime = f1_score(y_test, y_pred)
 
-    print('NaiveBayes:')
+    print("################################## " + classification_method + " ##################################")
     print('Accuracy: ' + str(accuracy))
     print('Precision: ' + str(precision))
     print('Recall: ' + str(recall))
-    print('F Score: ' + str(ftime))
 
 
 if __name__ == '__main__':
     process('sambafoot')
     X_train, X_test, y_train, y_test = bag_of_words()
     naive_bayes(X_train, X_test, y_train, y_test)
+    time.sleep(120)
+    decision_trees(X_train, X_test, y_train, y_test)
+    time.sleep(120)
+    support_vector_machine(X_train, X_test, y_train, y_test)
+    time.sleep(120)
+    logistic_regression(X_train, X_test, y_train, y_test)
+    time.sleep(120)
+    multilayer_perceptron(X_train, X_test, y_train, y_test)
